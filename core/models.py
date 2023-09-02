2c399289379b3ee116a37bda18915597eabad9cd
Mahdi Lotfian <80mahde@gmail.com>
2023-09-02 20:51:52 +0330

    add Book model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField()
+    price = models.IntegerField()
+    author = models.ForeignKey()
+    category = models.ForeignKey()
+    sell_count = models.IntegerField()
+    img = models.ImageField()
+    summary = models.TextField()
