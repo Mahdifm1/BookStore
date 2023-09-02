57b63d2f2e604aaaba68f8d0185d5b7ec570add0
Mahdi Lotfian <80mahde@gmail.com>
2023-09-02 21:07:50 +0330

    update core models

--- core/models.py
+++ core/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
 
-class Book(models.Model):
-    name = models.CharField()
-    price = models.IntegerField()
-    author = models.ForeignKey()
-    category = models.ForeignKey()
-    sell_count = models.IntegerField()
-    img = models.ImageField()
-    summary = models.TextField()
-
-
 class Author(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=30)
     age = models.IntegerField()
+    img = models.ImageField(upload_to="Authors/")
 
 
 class Category(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=30)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.IntegerField()
+    author = models.ForeignKey(Author, models.CASCADE)
+    category = models.ForeignKey(Category, models.CASCADE)
+    sell_count = models.IntegerField()
+    img = models.ImageField(upload_to="Books/")
+    summary = models.TextField()
