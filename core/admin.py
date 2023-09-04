36b3abef047e57fe418b41b3215c9262e69b2c83
Mahdi Lotfian <80mahde@gmail.com>
2023-09-04 21:45:47 +0330

    register models to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Book, Author, Category
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'author', 'category', 'sell_count',)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'number_of_books')
+
+    def number_of_books(self, author):
+        books = Book.objects.filter(author__exact=author)
+        return len(books)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number_of_books')
+
+    def number_of_books(self, category):
+        books = Book.objects.filter(category__exact=category)
+        return len(books)
--- core/models.py
+++ core/models.py
@@ -4,18 +4,27 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField()
-    img = models.ImageField(upload_to="Authors/")
+    img = models.ImageField(upload_to="images/Authors/", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Book(models.Model):
     name = models.CharField(max_length=60)
-    price = models.IntegerField()
+    price = models.IntegerField(null=False)
     author = models.ForeignKey(Author, models.CASCADE)
     category = models.ForeignKey(Category, models.CASCADE)
-    sell_count = models.IntegerField()
-    img = models.ImageField(upload_to="Books/")
+    sell_count = models.IntegerField(default=0)
+    img = models.ImageField(upload_to="images/Books/", null=True)
     summary = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
