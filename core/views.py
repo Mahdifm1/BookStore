07c08734a6d2a65d0d36b4a5598b43f114b61815
Mahdi Lotfian <80mahde@gmail.com>
2023-09-04 21:47:32 +0330

    add loading slider images from data

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Book
 
 
 class MainPage(View):
     def get(self, request):
-        return render(request, "core/main_page.html")
+        top_sellers = Book.objects.all().order_by('-sell_count')[:3]
+        print(top_sellers)
+        context = {'top_sell': top_sellers}
+        return render(request, "core/main_page.html", context)
