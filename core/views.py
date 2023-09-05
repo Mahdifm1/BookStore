53cb1a913253cc0e66db12641fa633f433dd2d1c
Mahdi Lotfian <80mahde@gmail.com>
2023-09-05 15:34:41 +0330

    send the latest books added data to template

--- core/views.py
+++ core/views.py
@@ -5,7 +5,17 @@ from .models import Book
 
 class MainPage(View):
     def get(self, request):
-        top_sellers = Book.objects.all().order_by('-sell_count')[:3]
-        print(top_sellers)
-        context = {'top_sell': top_sellers}
+        books = Book.objects.all()
+        top_sellers = books.order_by('-sell_count')[:3]
+        latest_products_query = list(books.order_by('-added_date'))[:12]
+        latest_products = []
+        pr = []
+        for i in range(len(latest_products_query)):
+            pr.append(latest_products_query[i])
+            if (i + 1) % 4 == 0:
+                latest_products.append(pr)
+                pr = []
+
+        context = {'top_sell': top_sellers,
+                   'latest_products': latest_products}
         return render(request, "core/main_page.html", context)
