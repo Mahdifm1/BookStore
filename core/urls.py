d3618760a5f220d5abc3e01a60a6cd5f9255b776
Mahdi Lotfian <80mahde@gmail.com>
2023-09-05 23:08:11 +0330

    add product detail to urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from core.views import MainPage
+from core.views import MainPage, BookDetail, ListCategories
 
 urlpatterns = [
     path('', MainPage.as_view(), name="main_page"),
+    path('books/<int:pk>', BookDetail.as_view(), name="book_detail"),
 ]
