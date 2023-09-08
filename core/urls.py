5eb1ff78ecb994966dc4c25d958f0e65ece2bcbf
Mahdi Lotfian <80mahde@gmail.com>
2023-09-08 18:52:35 +0330

    add authors page view to urls

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from core.views import MainPage, BookDetail, ListCategories
+from core.views import MainPage, BookDetail, ListCategories, AuthorsListView
 
 urlpatterns = [
     path('', MainPage.as_view(), name="main_page"),
     path('books/<int:pk>', BookDetail.as_view(), name="book_detail"),
     path('<str:name>', ListCategories.as_view(), name="list_categories"),
+    path('authors/', AuthorsListView.as_view(), name="list_authors")
 ]
