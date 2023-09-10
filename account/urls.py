e14a44a047fff2481a808b06327430925c1b4588
Mahdi Lotfian <80mahde@gmail.com>
2023-09-10 12:24:05 +0330

    add user dashboard view to urls

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from account.views import SignUp
+from account.views import SignUp, Dashboard
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("signup/", SignUp.as_view(), name='signup'),
+    path("dashboard/", Dashboard.as_view(), name="dashboard"),
 ]
