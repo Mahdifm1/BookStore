afe6037fc501095c935d128628fd6150eb1d95b5
Mahdi Lotfian <80mahde@gmail.com>
2023-09-10 14:32:37 +0330

    add about url

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from core.views import MainPage, BookDetail, ListCategories, AuthorsListView
+from core.views import MainPage, BookDetail, ListCategories, AuthorsListView, About
 
 urlpatterns = [
     path('', MainPage.as_view(), name="main_page"),
     path('books/<int:pk>', BookDetail.as_view(), name="book_detail"),
     path('<str:name>', ListCategories.as_view(), name="list_categories"),
-    path('authors/', AuthorsListView.as_view(), name="list_authors")
+    path('authors/', AuthorsListView.as_view(), name="list_authors"),
+    path('about/', About.as_view(), name='about'),
 ]
