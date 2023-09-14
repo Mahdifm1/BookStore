b2ee4647cd343dbe78c7275b410303e872daa03b
Mahdi Lotfian <80mahde@gmail.com>
2023-09-14 21:43:05 +0330

    update views for new template

--- core/views.py
+++ core/views.py
@@ -35,17 +35,11 @@ def get_categories():
 class MainPage(View):
     def get(self, request):
         books = Book.objects.all()
-        top_sellers = books.order_by('-sell_count')[:3]
-        latest_products_query = list(books.order_by('-added_date'))[:12]
-        latest_products = []
-        pr = []
-        for i in range(len(latest_products_query)):
-            pr.append(latest_products_query[i])
-            if (i + 1) % 4 == 0:
-                latest_products.append(pr)
-                pr = []
+        top_sellers = books.order_by('-sell_count')[:4]
+        latest_products = list(books.order_by('-added_date'))[:4]
 
         context = {'top_sell': top_sellers,
+                   'top_featured': top_sellers,
                    'latest_products': latest_products,
                    'top_categories': get_categories()}
         return render(request, "core/main_page.html", context)
