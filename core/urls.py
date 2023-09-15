cb84b83008240844c662c84de371c62af8c8b3be
Mahdi Lotfian <80mahde@gmail.com>
2023-09-15 23:28:09 +0330

    add boo slug to core urls

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from core.views import MainPage, BookDetail, ListCategories, AuthorsListView, Ab
 
 urlpatterns = [
     path('', MainPage.as_view(), name="main_page"),
-    path('books/<int:pk>', BookDetail.as_view(), name="book_detail"),
+    path('books/<slug:slug>', BookDetail.as_view(), name="book_detail"),
     path('<slug:category>', ListCategories.as_view(), name="list_categories"),
     path('authors/', AuthorsListView.as_view(), name="list_authors"),
     path('about/', About.as_view(), name='about'),
