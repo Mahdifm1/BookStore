2b1b7245b889d536622880af1aeaf26055361b99
Mahdi Lotfian <80mahde@gmail.com>
2023-09-19 01:36:08 +0330

    send data from blog list view to template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Blog
+from .models import Blog, Category
 
 
 class BlogList(ListView):
     model = Blog
     template_name = 'blog/blog_list.html'
+    paginate_by = 5
+
+    # context
+    posts = Blog.objects.all().order_by('added_date')
+    recent_posts = posts[:3]
+    categories = Category.objects.all()
+    extra_context = {
+        'posts': posts,
+        'recent_posts': recent_posts,
+        'categories': categories
+    }
