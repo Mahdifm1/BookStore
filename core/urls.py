e153c836b98b7a670465d2a7702db7100b06e405
Mahdi Lotfian <80mahde@gmail.com>
2023-09-18 21:08:58 +0330

    add top category and sub category to core urls

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', MainPage.as_view(), name="main_page"),
     path('books/<slug:slug>', BookDetail.as_view(), name="book_detail"),
     path('<slug:category>', ListCategories.as_view(), name="list_categories"),
+    path('<slug:category>/<slug:sub_category>', ListCategories.as_view(), name="list_categories"),
     path('authors/', AuthorsListView.as_view(), name="list_authors"),
     path('about/', About.as_view(), name='about'),
     path('contact-us/', ContactUs.as_view(), name='contact_us'),
