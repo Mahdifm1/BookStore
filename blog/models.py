4bc7e24cdf55582279a49772352a7a3b4859fc77
Mahdi Lotfian <80mahde@gmail.com>
2023-09-18 23:58:06 +0330

    add Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core.models import Category
 
-# Create your models here.
+
+class Comment(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='images/Blog/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, models.CASCADE)
+    created_date = models.DateField(auto_now_add=True)
