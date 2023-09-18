1dfef2080a650ee58e6c7226f463110e82a888de
Mahdi Lotfian <80mahde@gmail.com>
2023-09-19 01:52:34 +0330

    add blog and tag model to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Blog)
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Tag)
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
