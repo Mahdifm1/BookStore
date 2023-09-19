640a0ec544db3beaa08ebb0fd24bacd1da9df56d
Mahdi Lotfian <80mahde@gmail.com>
2023-09-20 01:47:47 +0330

    add is accepted to comment model

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,8 @@ class Blog(models.Model):
 
 class Comment(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    added_date = models.DateField(auto_now_add=True)
+    added_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField(max_length=150)
     name = models.CharField(max_length=30)
     email = models.EmailField()
+    is_accepted = models.BooleanField(default=False)
