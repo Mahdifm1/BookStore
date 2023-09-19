f1e68e4c58ec8a40185705a93314f94c641e25aa
Mahdi Lotfian <80mahde@gmail.com>
2023-09-19 13:37:13 +0330

    add post view to urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogList
+from .views import BlogList,Post
 
 urlpatterns = [
     path('', BlogList.as_view(), name='blog list'),
+    path('<slug:slug>', Post.as_view(), name='post'),
 ]
